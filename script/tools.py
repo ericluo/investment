840b0ec8cc733ad0769ef9f17db57ce7d5759e84
wenbl <3944134@qq.com>
2017-04-02 08:01:22 +0800

    runnable

--- script/tools.py
+++ script/tools.py
@@ -1,7 +1,16 @@
+import os
 
-with open("../docs/balance_sheet_doc.txt", "rt", encoding='utf8') as f:
-    lines = [l.strip() for l in f.readlines()]
+stmts = ["balance_sheet.txt", "income_statement.txt", "cash_flow_statement.txt"]
+kws = ["bs", "is", "cs"]
 
-    nums = int(len(lines) / 2)
-    s = ["('{l}', bs.{v})".format(l=lines[i * 2 + 1], v=lines[i * 2]) for i in range(nums)]
-    print(", ".join(s))
+ofn = os.path.relpath("docs/subject_mapper.txt", __file__)
+with open(ofn,  "wt", encoding='utf8') as outf:
+    for stmt, kw in zip(stmts, kws):
+        ifn = os.path.relpath(f"docs/{stmt}", __file__)
+        with open(ifn, "rt", encoding='utf8') as inf:
+            subject_exps = list()
+            for line in inf:
+                sub, label = line.strip().split(" ")
+                subject_exps.append(f"('{label.strip()}', {kw}.{sub.strip()})")
+            outf.write("[" + ", ".join(subject_exps) + "]")
+            outf.write("\n")
